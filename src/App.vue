cba6ba7cc5577a781ab3b34a83d04d99b165fa9e
nir-st <nirstein@post.bgu.ac.il>
2021-06-14 18:43:58 +0300

    updated navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand :to="{ name: 'main' }">Superliga Vue</b-navbar-brand>
+      <b-navbar-brand :to="{ name: 'main' }">Superliga</b-navbar-brand>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-
         <b-nav-item :to="{ name: 'search' }">Search</b-nav-item>
+        <b-nav-item :to="{ name: 'currentSeries' }">Current Series</b-nav-item>
+        <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
+        <b-nav-item-dropdown text="Personal" v-if="$root.store.username">
+          <b-dropdown-item>My Teams</b-dropdown-item>
+          <b-dropdown-item>My Players</b-dropdown-item>
+          <b-dropdown-item>My Games</b-dropdown-item>
+        </b-nav-item-dropdown>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto" v-if="!$root.store.username">
           <b-nav-item :to="{ name: 'login' }">Login</b-nav-item>
           <b-nav-item :to="{ name: 'register' }">Register</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto" v-else>
-        <b-nav-item-dropdown right>
-          <template #button-content>
-            User
-          </template>
-          <b-dropdown-item href="#">Favorites</b-dropdown-item>
-          <b-dropdown-item href="#">Log Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <b-nav-item>
+          {{ $root.store.username }}
+        </b-nav-item>
+        <b-nav-item>Logout</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
