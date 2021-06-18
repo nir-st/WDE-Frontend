0079241c7206a0501a3fad40a1aba51a9a14cbea
nir-st <nirstein@post.bgu.ac.il>
2021-06-18 21:55:50 +0300

    can logout

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
             User
           </template>
           <b-dropdown-item href="#">Favorites</b-dropdown-item>
-          <b-dropdown-item href="#">Log Out</b-dropdown-item>
+          <b-dropdown-item href="#" @click="Logout">Log Out</b-dropdown-item>
         </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -30,10 +30,12 @@
 export default {
   name: "App",
   methods: {
-    Logout() {
+    async Logout() {
       this.$root.store.logout();
       this.$root.toast("Logout", "User logged out successfully", "success");
-
+      await this.axios.post(
+          "http://localhost:3000/logout",
+        );
       this.$router.push("/").catch(() => {
         this.$forceUpdate();
       });
