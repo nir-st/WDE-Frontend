05660065affee24a8d443d793e734d5b6636e6b9
nir-st <nirstein@post.bgu.ac.il>
2021-07-08 23:04:35 +0300

    style&data, no next game

--- src/components/LeagueInfo.vue
+++ src/components/LeagueInfo.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="league-preview">
+  <div class="league-preview">
       <b-card
       img-alt="Image"
       tag="article"
       style="max-width: 20rem;"
       class="mb-2"
+      bg-variant="dark"
+      text-variant="white"
     >
       <b-card-title>{{leagueName}}</b-card-title>
       <b-card-text>
@@ -12,7 +14,6 @@
         <br/>
         Stage: {{ stage }}
       </b-card-text>
-      <b-button href="#" variant="primary">Go somewhere</b-button>
     </b-card>
   </div>
 </template>
@@ -26,6 +27,23 @@ export default {
       stage: "stage"
     };
   },
+  methods: {
+    async getData() {
+      try {
+        const response = await this.axios.get(
+          `http://localhost:3000/league/getDetails`
+        );
+      this.leagueName = response.data.league_name;
+      this.season = response.data.current_season_name;
+      this.stage = response.data.current_stage_name;
+      } catch(err) {
+        console.log(err);
+      }
+    }
+  },
+  mounted() {
+    this.getData();
+  }
 }
 </script>
 
@@ -40,6 +58,11 @@ export default {
   border-radius: 10px;
   border-width: 5px;
   border-color:rgb(44, 89, 116);
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.b-card {
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
 .league-preview .league-title {
