c6d858414ca5f3af3152e5be7c4aed0a48d8be3f
nir-st <nirstein@post.bgu.ac.il>
2021-07-09 13:34:14 +0300

    can search players

---
+++ src/components/PlayerPreview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="player-preview">
+  <b-card
+    no-body
+    style="max-width: 20rem;"
+    v-bind:img-src="imageUrl"
+    img-top
+  >
+    <template #header>
+      <h4 class="mb-0">{{fullname}}</h4>
+    </template>
+
+    <b-card-body>
+      <b-card-title>{{teamname}}</b-card-title>
+      <b-card-sub-title class="mb-2">position id: {{position}}</b-card-sub-title>
+    </b-card-body>
+  </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    fullname: String, 
+    teamname: String,
+    position: Number,
+    imageUrl: String
+  }
+}
+</script>
+
+<style>
+</style>
