21875cd842df72070fd35b6fa2d1199971a726bd
nir-st <nirstein@post.bgu.ac.il>
2021-07-09 23:14:36 +0300

    games page

--- src/components/GamePreview.vue
+++ src/components/GamePreview.vue
@@ -4,10 +4,20 @@
       <b>Game Id:</b> {{ id }}
     </div>
     <ul class="game-content">
-      <li> host: {{ hostTeam }}</li>
-      <li> guest: {{ guestTeam }}</li>
-      <li> date: {{ date }}</li>
-      <li> time: {{ hour }}</li>
+      <li> <b>host:</b> 
+        <router-link :to="{name:'team', params:{id: this.hostTeamId} }">
+          {{ this.hostTeamName }}
+        </router-link>
+      </li>
+      <li> <b>guest:</b>
+        <router-link :to="{name:'team', params:{id: this.hostTeamId} }">
+          {{ this.guestTeamName }}
+        </router-link>
+      </li>
+      <li> <b>date:</b> {{ date }}</li>
+      <li> <b>time:</b> {{ hour }}</li>
+      <li> <b>stadium:</b> {{ stadium }}</li>
+      <li> <b>referee:</b> {{ referee }}</li>
     </ul>
   </div>
 </template>
@@ -20,12 +30,12 @@ export default {
         type: Number,
         required: true
       },
-      hostTeam: {
-        type: String,
+      hostTeamId: {
+        type: Number,
         required: true
       },
-      guestTeam: {
-        type: String,
+      guestTeamId: {
+        type: Number,
         required: true
       },
       date: {
@@ -35,10 +45,47 @@ export default {
       hour: {
         type: String,
         required: true
+      },
+      stadium: {
+        type: String,
+        required: true
+      },
+      referee: {
+        type: String,
+        required: true
       }
-  }, 
+  },
+  data() {
+    return {
+      hostTeamName: "",
+      guestTeamName: ""
+    }
+  },
+  methods: {
+    async getTeamNames() {
+      const nameHome = await this.axios.get(
+        `http://localhost:3000/teams/teamName/${this.hostTeamId}`
+      );
+      if (nameHome && nameHome.data) {
+        this.hostTeamName = nameHome.data;
+      }
+      else {
+        this.hostTeamName = "team name not found";
+      }
+
+      const nameAway = await this.axios.get(
+        `http://localhost:3000/teams/teamName/${this.guestTeamId}`
+      );
+      if (nameAway && nameAway.data) {
+        this.guestTeamName = nameAway.data;
+      }
+      else {
+        this.guestTeamName = "team name not found";
+      }
+    }
+  },
   mounted(){
-    console.log("game preview mounted")
+    this.getTeamNames();
   } 
 };
 </script>
@@ -46,8 +93,8 @@ export default {
 <style>
 .game-preview {
   display: inline-block;
-  width: 250px;
-  height: 200px;
+  width: 280px;
+  height: 300px;
   position: relative;
   margin: 10px 10px;
   border-style: solid;
---
+++ src/pages/GamesPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <center>
+    <div class="outer">
+      <b-card no-body>
+        <b-tabs v-model="tabIndex" card align="center">
+          <b-tab title="Future Games" :title-link-class="linkClass(0)">
+              <GamePreview v-for="game in futureGames" :key="game.gameId"
+                :id="game.gameId"
+                :hostTeamId="game.homeTeamId"
+                :guestTeamId="game.awayTeamId"
+                :date="game.date"
+                :hour="game.time"
+                :stadium="game.stadium"
+                :referee="game.referee">
+              </GamePreview>
+          </b-tab>
+          <b-tab title="Past Games" :title-link-class="linkClass(1)">all past games here</b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+  </center>
+</template>
+
+<script>
+import GamePreview from '../components/GamePreview.vue';
+export default {
+  components: {
+      GamePreview
+  },
+  data() {
+    return {
+      tabIndex: 0,
+      futureGames: [],
+      pastGames: [],
+      isFuture: false,
+      isPast: false
+    }
+  },
+  mounted() {
+      this.getData();
+  },
+  methods: {
+    async getData() {
+      const future = await this.axios.get(
+          `http://localhost:3000/games/allFutureGames`
+      );
+      const past = await this.axios.get(
+          `http://localhost:3000/games/allFutureGames`
+      );
+      if (future.data && future.data.length > 0) {
+        this.futureGames = future.data;
+        this.isFuture = true;
+      }
+      else { this.isFuture = false; }
+      if (past.data && past.data.length > 0) {
+        this.pastGames = past.data;
+        this.isPast = true;
+      }
+      else { this.isPast = false; }
+    },
+    linkClass(idx) {
+      if (this.tabIndex === idx) {
+        return ['bg-primary', 'text-light']
+      } else {
+        return ['bg-light', 'text-info']
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.outer {
+    width: 80%;
+    margin-top: 20px;
+    padding-top: 20px;
+}
+</style>
