fe053862efd12d7675f4ff998fa51f6b6de931bf
nir-st <nirstein@post.bgu.ac.il>
2021-07-10 17:37:00 +0300

    hello guest

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,10 @@
 
         <b-nav-item :to="{ name: 'search' }">Search</b-nav-item>
         <b-nav-item :to="{ name: 'games' }">Games</b-nav-item>
+        <b-nav-item href="#">About</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto" v-if="!$root.store.username">
+          <b-nav-item>Hello guest!         </b-nav-item>
           <b-nav-item :to="{ name: 'login' }">Login</b-nav-item>
           <b-nav-item :to="{ name: 'register' }">Register</b-nav-item>
         </b-navbar-nav>
